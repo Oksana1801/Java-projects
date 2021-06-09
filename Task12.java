41110a764d670973a0ad6be84baafff87883b663
Oksana1801 <85490316+Oksana1801@users.noreply.github.com>
2021-06-09 16:11:10 +0300

    Add files via upload

---
+++ Task12.java
@@ -0,0 +1,11 @@
+package work2;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println(" ***        *      *           *          *");
+        System.out.println("    *      * *      *         *         *  *");
+        System.out.println("    *     *   *      *       *         *    *" );
+        System.out.println("*   *    *******      *     *         ********");
+        System.out.println(" ***    *       *      * * *         *        *");
+    }
+}
