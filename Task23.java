a19603829bc860ca06db8eaca66124e979f3a644
Oksana1801 <85490316+Oksana1801@users.noreply.github.com>
2021-06-13 18:08:55 +0300

    Add files via upload

---
+++ Task23.java
@@ -0,0 +1,15 @@
+package work23;
+
+public class Task23 {
+    public static void main(String[] args) {
+        int value1 = 21345;
+        int value2 = -78;
+
+        value1 = value1 + value2;
+        value2 = value1 - value2;
+        value1 = value1 - value2;
+
+        System.out.println("value1 = " + value1 + "\nvalue2 = " + value2);
+
+    }
+}
